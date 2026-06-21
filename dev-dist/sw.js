6a0011365378693dfddf925f059c0be3c86e706a
vrahul2698 <vrahulmba005@gmail.com>
2026-06-21 20:32:51 +0530

    Added login updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9l4lr3jdc9g"
+    "revision": "0.ldjit4niiag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
